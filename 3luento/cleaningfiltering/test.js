8a9f2a8f6a5f7ea960bade4aa3808d2844aab43e
Maarit Välisuo <mvalisuo@MacBookAir.lan>
2024-11-06 08:52:29 +0200

    object to CSV esimerkki

--- 3luento/cleaningfiltering/test.js
+++ 3luento/cleaningfiltering/test.js
@@ -1,6 +1,3 @@
+const planets = require('./planets.json')
 
-const str = '[{"id" : 114, "name" : "Misse"}, {"id" : 115, "name" : "M\'aam"}]'
-
-const kissat = JSON.parse(str)
-
-console.log(kissat.length)
+console.log(planets)
